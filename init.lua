e07c0de2be9893790d9a76c00706851b4db32d15
Victor Martin <vicover90@gmail.com>
2024-12-06 17:18:07 +0100

    Add no completion auto on enter, valid  autcompletion with CTR+Y.

--- init.lua
+++ init.lua
@@ -4,3 +4,13 @@ require("config.lazy")
 -- Copy shortcut
 vim.api.nvim_set_keymap('v', '<C-c>', ':w !xclip -selection clipboard<CR><CR>', { noremap = true, silent = true })
 
+
+require('neo-tree').setup {
+  filesystem = {
+    filtered_items = {
+      visible = true, -- This will show hidden files
+      hide_dotfiles = false, -- This will show dotfiles
+      hide_gitignored = false, -- This will show gitignored files
+    },
+  },
+}
--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -34,7 +34,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
+    --- @param opts cmp.ConfigSchema
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
     end,
